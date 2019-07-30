172e0ca7267c8e36530af78d5e0f9eae1be4ac31
Kyrylo Glamazdin <kirill_glamazdin@iMac-Kyrylo.home>
2019-07-29 23:58:30 -0400

    Updates

---
+++ precache-manifest.f5c503be9ea84833af6d6e4e6275a2cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def27211fd5449c3c407030808c31a02",
+    "url": "/Bank_Assignment/index.html"
+  },
+  {
+    "revision": "9323337a30c9e41e4cb0",
+    "url": "/Bank_Assignment/static/css/main.d75989a4.chunk.css"
+  },
+  {
+    "revision": "4819cec091cbba0d3f3f",
+    "url": "/Bank_Assignment/static/js/2.4d2a423c.chunk.js"
+  },
+  {
+    "revision": "9323337a30c9e41e4cb0",
+    "url": "/Bank_Assignment/static/js/main.3bcf1fe2.chunk.js"
+  },
+  {
+    "revision": "9521d4df0cb0966f7e85",
+    "url": "/Bank_Assignment/static/js/runtime~main.4836e76b.js"
+  }
+]);
